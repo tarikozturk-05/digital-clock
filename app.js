3c31a09f38389852aec7c7f57c07ed39f47c1fbb
Tarik oz <a.tarik2005@gmail.com>
2022-09-29 14:40:05 +0300

    fixed fixed

--- app.js
+++ app.js
@@ -7,9 +7,26 @@ const zaman = ()=>{
         let second = new Date().getSeconds();
         let hour = new Date().getHours();
         let minute = new Date().getMinutes();
-        saat.innerHTML = `${hour}`
-        dak.innerHTML = `${second}`;
-        saniye.innerHTML = `${minute}`;
+       
+if(hour<13){
+    pa.innerHTML= `AM`;
+     saat.innerHTML = `${hour}`;
+     dak.innerHTML = `${minute}`;
+     saniye.innerHTML = `${second}`;
+}else{
+    pa.innerHTML = `PM`;
+     if(hour -12 <10){
+         saat.innerHTML = `0${hour -12}`;
+     }else{
+         saat.innerHTML = `${hour -12}`;
+     }
+     dak.innerHTML = `${minute}`;
+     saniye.innerHTML = `${second}`;
+}
+
 }
 setInterval(zaman, 1000);
-// window.onload()
+window.onload = () => {zaman()}
+
+
+//? A.M ve P.M kısaltmaları Latince’den alınmışlardır. A.M yani “Ante Meridiem”, “öğleden önce” anlamına gelirken; P.M yani “Post Meridiem” ise “öğleden sonra” anlamına gelir.
